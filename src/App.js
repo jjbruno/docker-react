ca9629fd8480619cee45c00d7db920433994af8b
JJ Bruno <brujj77@gmail.com>
2020-03-03 20:07:01 -0800

    Change for feature branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn react - New feature branch.
         </a>
       </header>
     </div>
